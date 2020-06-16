471c825c7d03ed6e25d831721e22dee5634c1bc5
rameshvk <ramesh.vyaghrapuri@gmail.com>
2020-06-16 00:06:25 -0700

    Enhance Func support (#19)
    Co-authored-by: Ramesh VK <ramesh@do_not_email_me.com>

--- pkg/script/func.go
+++ pkg/script/func.go
@@ -6,26 +6,39 @@ import (
 )
 
 type fn struct {
-	f       func(ctx context.Context) error
-	started bool
+	f      func(ctx context.Context, r io.Reader, w io.Writer) error
+	ch     chan error
+	stdin  io.Reader
+	stdout io.Writer
 }
 
 func (f *fn) Start(ctx context.Context) error {
-	f.started = true
+	f.ch = make(chan error, 1)
+	go func() {
+		var err error
+		defer func() {
+			f.ch <- err
+		}()
+		err = f.f(ctx, f.stdin, f.stdout)
+	}()
 	return nil
 }
 
 func (f fn) Wait(ctx context.Context) error {
-	if f.started {
-		return f.f(ctx)
+	if f.ch != nil {
+		return <-f.ch
 	}
 	return nil
 }
 
 func (f fn) Stdin(r io.Reader) Task {
-	panic("cannot pipe into to a function")
+	result := f
+	result.stdin = r
+	return &result
 }
 
 func (f fn) Stdout(w io.Writer) Task {
-	panic("cannot pipe out of a function")
+	result := f
+	result.stdout = w
+	return &result
 }
